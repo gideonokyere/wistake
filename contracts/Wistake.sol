1ea99f0c727206b435611f3be13b03bc6e5193f5
gideonokyere <okyeregideon10@gmail.com>
2024-03-18 09:19:51 +0000

    console importing remove

--- contracts/Wistake.sol
+++ contracts/Wistake.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
-import "hardhat/console.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
